27c54d6f5ec27a9c4aaa9fb389157633247edcef
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-06-15 10:57:09 +0200

    addu32

--- klee_common.hpp
+++ klee_common.hpp
@@ -15,4 +15,10 @@ inline int32_t clamp32 (int64_t const x) {
       static_cast<int64_t> (std::numeric_limits<int32_t>::max ())));
 }
 
+inline uint32_t clampu32 (uint64_t const x) {
+  return static_cast<uint32_t> (std::clamp (
+      x, static_cast<uint64_t> (std::numeric_limits<uint32_t>::min ()),
+      static_cast<uint64_t> (std::numeric_limits<uint32_t>::max ())));
+}
+
 #endif  // KLEE_COMMON_HPP
