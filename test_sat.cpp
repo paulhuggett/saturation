1f97539c21057ceb7dead007daa11bc7421df5bb
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-06-16 15:57:00 +0200

    unsigned unit tests.

--- test_sat.cpp
+++ test_sat.cpp
@@ -6,6 +6,7 @@ class Saturation : public testing::Test {
 protected:
   static constexpr auto min = std::numeric_limits<int32_t>::min ();
   static constexpr auto max = std::numeric_limits<int32_t>::max ();
+  static constexpr auto maxu = std::numeric_limits<uint32_t>::max ();
 };
 
 TEST_F (Saturation, Adds32) {
@@ -40,4 +41,25 @@ TEST_F (Saturation, Muls32) {
   EXPECT_EQ (sat::muls32 (int32_t{3}, int32_t{-5}), int32_t{-15});
   EXPECT_EQ (sat::muls32 (int32_t{3}, int32_t{-1342177280}), min);
   EXPECT_EQ (sat::muls32 (int32_t{3}, int32_t{997556224}), max);
+}
+
+TEST_F (Saturation, Addu32) {
+  EXPECT_EQ (sat::addu32 (uint32_t{0}, uint32_t{0}), uint32_t{0});
+  EXPECT_EQ (sat::addu32 (maxu, uint32_t{1}), maxu);
+}
+
+TEST_F (Saturation, Subu32) {
+  EXPECT_EQ (sat::subu32 (uint32_t{0}, uint32_t{0}), uint32_t{0});
+  EXPECT_EQ (sat::subu32 (uint32_t{0}, uint32_t{1}), uint32_t{0});
+}
+
+TEST_F (Saturation, Divu32) {
+  EXPECT_EQ (sat::divu32 (uint32_t{0}, uint32_t{16843009}), uint32_t{0});
+}
+
+TEST_F (Saturation, Mulu32) {
+  EXPECT_EQ (sat::mulu32 (uint32_t{0}, uint32_t{0}), uint32_t{0});
+  EXPECT_EQ (sat::mulu32 (uint32_t{3}, uint32_t{5}), uint32_t{15});
+  EXPECT_EQ (sat::mulu32 (uint32_t{13862387}, uint32_t{1076719596}),
+             uint32_t{4294967295});
 }
