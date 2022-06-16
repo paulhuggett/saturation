b4a2606e0f16a93315f758d49b416bd035d7813b
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-06-16 09:49:41 +0200

    adds32/subs32 unit tests.

--- test_sat.cpp
+++ test_sat.cpp
@@ -2,5 +2,21 @@
 
 #include "sat.hpp"
 
-TEST (a, b) {
+TEST (Saturation, Adds32) {
+  constexpr auto min = std::numeric_limits<int32_t>::min();
+  constexpr auto max = std::numeric_limits<int32_t>::max();
+  EXPECT_EQ (sat::adds32(int32_t{0}, int32_t{0}), int32_t{0});
+  EXPECT_EQ (sat::adds32(int32_t{3}, int32_t{5}), int32_t{8});
+  EXPECT_EQ (sat::adds32(min, min), min);
+  EXPECT_EQ (sat::adds32(max, max), max);
+  EXPECT_EQ (sat::adds32(int32_t{16777216}, int32_t{2130706432}), int32_t{2147483647});
+}
+
+TEST (Saturation, Subs32) {
+  constexpr auto min = std::numeric_limits<int32_t>::min();
+  constexpr auto max = std::numeric_limits<int32_t>::max();
+  EXPECT_EQ (sat::subs32(int32_t{0}, int32_t{0}), int32_t{0});
+  EXPECT_EQ (sat::subs32(min, int32_t{1}), min);
+  EXPECT_EQ (sat::subs32(0, min), max);
+  EXPECT_EQ (sat::subs32(max, max), int32_t{0});
 }
