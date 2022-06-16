d61bb658aabb4d63b728edba12390fe6949481f9
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-06-16 11:13:24 +0200

    use auto.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 
 #include "sat.hpp"
 
-int main (int argc, const char* argv[]) {
-  int32_t const a = std::numeric_limits<int32_t>::max () - 1;
-  int32_t const b = int32_t{2};
-  int32_t const c = sat::adds32 (a, b);
+int main (int argc, char const* argv[]) {
+  auto const a = std::numeric_limits<int32_t>::max () - 1;
+  auto const b = int32_t{2};
+  auto const c = sat::adds32 (a, b);
   std::cout << std::hex << c << '\n';
 }
