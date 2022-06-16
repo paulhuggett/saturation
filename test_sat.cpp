7264b65b88fff34b3bcf74a9195c21143df72391
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-06-16 10:57:14 +0200

    Extend unit tests.

--- test_sat.cpp
+++ test_sat.cpp
@@ -2,21 +2,42 @@
 
 #include "sat.hpp"
 
-TEST (Saturation, Adds32) {
-  constexpr auto min = std::numeric_limits<int32_t>::min();
-  constexpr auto max = std::numeric_limits<int32_t>::max();
-  EXPECT_EQ (sat::adds32(int32_t{0}, int32_t{0}), int32_t{0});
-  EXPECT_EQ (sat::adds32(int32_t{3}, int32_t{5}), int32_t{8});
-  EXPECT_EQ (sat::adds32(min, min), min);
-  EXPECT_EQ (sat::adds32(max, max), max);
-  EXPECT_EQ (sat::adds32(int32_t{16777216}, int32_t{2130706432}), int32_t{2147483647});
+class Saturation : public testing::Test {
+protected:
+  static constexpr auto min = std::numeric_limits<int32_t>::min ();
+  static constexpr auto max = std::numeric_limits<int32_t>::max ();
+};
+
+TEST_F (Saturation, Adds32) {
+  EXPECT_EQ (sat::adds32 (int32_t{0}, int32_t{0}), int32_t{0});
+  EXPECT_EQ (sat::adds32 (int32_t{3}, int32_t{5}), int32_t{8});
+  EXPECT_EQ (sat::adds32 (min, min), min);
+  EXPECT_EQ (sat::adds32 (max, max), max);
+  EXPECT_EQ (sat::adds32 (int32_t{16777216}, int32_t{2130706432}), max);
 }
 
-TEST (Saturation, Subs32) {
-  constexpr auto min = std::numeric_limits<int32_t>::min();
-  constexpr auto max = std::numeric_limits<int32_t>::max();
-  EXPECT_EQ (sat::subs32(int32_t{0}, int32_t{0}), int32_t{0});
-  EXPECT_EQ (sat::subs32(min, int32_t{1}), min);
-  EXPECT_EQ (sat::subs32(0, min), max);
-  EXPECT_EQ (sat::subs32(max, max), int32_t{0});
+TEST_F (Saturation, Subs32) {
+  EXPECT_EQ (sat::subs32 (int32_t{0}, int32_t{0}), int32_t{0});
+  EXPECT_EQ (sat::subs32 (int32_t{0}, int32_t{1}), int32_t{-1});
+  EXPECT_EQ (sat::subs32 (min, int32_t{1}), min);
+  EXPECT_EQ (sat::subs32 (0, min), max);
+  EXPECT_EQ (sat::subs32 (max, int32_t{0}), max);
+  EXPECT_EQ (sat::subs32 (max, max), int32_t{0});
+  EXPECT_EQ (sat::subs32 (max, int32_t{1}), int32_t{max - 1});
+}
+
+TEST_F (Saturation, Divs32) {
+  EXPECT_EQ (sat::divs32 (int32_t{0}, int32_t{1}), int32_t{0});
+  EXPECT_EQ (sat::divs32 (int32_t{4}, int32_t{2}), int32_t{2});
+  EXPECT_EQ (sat::divs32 (max, int32_t{2}), int32_t{max / 2});
+  EXPECT_EQ (sat::divs32 (min, int32_t{-1}), max);
+}
+
+TEST_F (Saturation, Muls32) {
+  EXPECT_EQ (sat::muls32 (int32_t{0}, int32_t{0}), int32_t{0});
+  EXPECT_EQ (sat::muls32 (int32_t{3}, int32_t{5}), int32_t{15});
+  EXPECT_EQ (sat::muls32 (int32_t{-3}, int32_t{5}), int32_t{-15});
+  EXPECT_EQ (sat::muls32 (int32_t{3}, int32_t{-5}), int32_t{-15});
+  EXPECT_EQ (sat::muls32 (int32_t{3}, int32_t{-1342177280}), min);
+  EXPECT_EQ (sat::muls32 (int32_t{3}, int32_t{997556224}), max);
 }
