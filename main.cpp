7b421cde8a1eb842984885d8b4376d2884e68038
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-06-26 17:14:33 +0200

    main() arguments are unused.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include "saturation.hpp"
 
-int main (int argc, char const* argv[]) {
+int main () {
   auto const a = std::numeric_limits<int32_t>::max () - 1;
   auto const b = int32_t{2};
   auto const c = sat::adds32 (a, b);
