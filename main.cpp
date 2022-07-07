6d780bbdb0dc63ad989aea24aff1484bb3dd16e0
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-07-07 16:17:09 +0200

    Extend the message so that it might make more sense.

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main () {
   auto const a = std::numeric_limits<int32_t>::max () - 1;
   auto const b = int32_t{2};
   auto const c = saturation::adds32 (a, b);
-  std::cout << std::hex << c << '\n';
+  std::cout << "Saturating signed 32-bit add: " << std::hex << a << " + " << b
+            << " = " << c << '\n';
 }
