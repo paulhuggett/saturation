178b5613270dd2e5292e18acc07dbd56bce96f60
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-08-18 21:32:24 +0200

    Add extra doxygen-generated files.

---
+++ docs/doxygen/html/search/functions_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pop_5fcount_0',['pop_count',['../saturation_8hpp.html#acaf663943d4f2e7336618547e6a075c1',1,'saturation::details']]]
+];
---
+++ docs/doxygen/html/search/functions_7.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['subs_0',['subs',['../saturation_8hpp.html#a4a6e2f5418aedfc8e0f1f60501fd1411',1,'saturation']]],
+  ['subs16_1',['subs16',['../saturation_8hpp.html#ac23b6b0273dd429607630aefd40610c1',1,'saturation']]],
+  ['subs32_2',['subs32',['../saturation_8hpp.html#a22e9c5913914a1a45302a70c374aef51',1,'saturation']]],
+  ['subs8_3',['subs8',['../saturation_8hpp.html#a6c523fae2d5f8d17280195e925f08fc8',1,'saturation']]],
+  ['subu_4',['subu',['../saturation_8hpp.html#a5158b7680d4b465f34edbfd8b8a08498',1,'saturation']]],
+  ['subu16_5',['subu16',['../saturation_8hpp.html#ac8f333ee4039141f9a2e9bc2ef52de60',1,'saturation']]],
+  ['subu32_6',['subu32',['../saturation_8hpp.html#a22e744f6f7792ac346b2411fcc6bc522',1,'saturation']]],
+  ['subu8_7',['subu8',['../saturation_8hpp.html#a116d1768149c1eca6cf285bcd21affb4',1,'saturation']]]
+];
