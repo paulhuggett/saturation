1ae6c0a0d08454388c00e054236d920359c83fec
Paul Bowen-Huggett <paulhuggett@mac.com>
2022-08-28 16:18:47 +0200

    Reduce work done on redraw. Move all styling to CSS.

--- docs/mixer.js
+++ docs/mixer.js
@@ -1,142 +1,129 @@
 import * as d3 from 'https://cdn.skypack.dev/d3@7'
 
-const width = 650 / 2
-const margin = { top: 12, left: 30, bottom: 12, right: 4 }
-const internalWidth = width - margin.left - margin.right
-let clipNum = 1
-
 function getYDomain (data) {
   const yMax = Math.ceil(d3.max(data, d => Math.abs(d[1])))
   return [-yMax, yMax]
 }
+function radiansToUser (v) {
+  const format = d3.format('d')
+  return format(v * (180 / Math.PI)) + '°'
+}
+function sine (id, index) {
+  const margin = { top: 12, left: 30, bottom: 12, right: 12 }
 
-function sine (id, data) {
-  const height = d3.select(id).select('svg').attr('height')
-  const internalHeight = height - margin.top - margin.bottom
+  const svg = d3.select(id).select('svg')
+  const width = +svg.attr('width') - margin.left - margin.right
+  const height = +svg.attr('height') - margin.top - margin.bottom
 
-  d3.select(id).selectChildren().remove()
-  const svg = d3.select(id)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
+  const xDomain = [0, Math.PI * 2]
+  const xScale = d3.scaleLinear().range([0, width]).domain(xDomain)
+  const xAxis = d3.axisBottom().scale(xScale)
+    .tickValues(Array(4).fill(0).map((_, i) => (i + 1) / 2 * Math.PI))
+    .tickFormat(radiansToUser)
+  const yAxis = d3.axisLeft().ticks(3)
 
-  const xDomain = d3.extent(data, d => d[0])
-  const yDomain = getYDomain(data)
+  const clipPos = svg.select(`#clip-${index}-pos`).attr('x', xScale(0)).attr('width', xScale(xDomain[1]) - xScale(0))
+  const clipNeg = svg.select(`#clip-${index}-neg`).attr('x', xScale(0)).attr('width', xScale(xDomain[1]) - xScale(0))
 
-  const xScale = d3.scaleLinear().range([0, internalWidth]).domain(xDomain)
-  const yScale = d3.scaleLinear().range([internalHeight, 0]).domain(yDomain)
-  const xAxis = d3.axisBottom().scale(xScale).tickValues([1, 2, 3, 4, 5, 6])
-  const yAxis = d3.axisLeft().scale(yScale).ticks(3)
+  const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`)
+  const maxLine = g.append('line').classed('limit-line', true)
+  const minLine = g.append('line').classed('limit-line', true)
+
+  const path = g.append('path').classed('line', true)
+  const fill = g.append('path').classed('fill', true)
+
+  g.append('g').classed('axis', true).attr('transform', `translate(0, ${height / 2})`).call(xAxis)
+  const yAxisEl = g.append('g').classed('yaxis', true)
 
   const line = d3.line()
     .x(d => xScale(d[0]))
-    .y(d => yScale(d[1]))
 
-  const clipId = 'clip' + clipNum++
-  const clipGt1 = svg.append('clipPath')
-    .attr('id', clipId)
-  clipGt1.append('rect')
-    .attr('x', xScale(0))
-    .attr('y', yScale(yDomain[1]))
-    .attr('width', xScale(xDomain[1]) - xScale(0))
-    .attr('height', yScale(1) - yScale(yDomain[1]))
-  clipGt1.append('rect')
-    .attr('x', xScale(0))
-    .attr('y', yScale(-1))
-    .attr('width', xScale(xDomain[1]) - xScale(0))
-    .attr('height', yScale(yDomain[0]) - yScale(-1))
+  svg.selectAll('.loading').remove()
 
-  const g = svg.append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-  // Are we showing clipped output?
-  if (yDomain[0] < -1.0 || yDomain[1] > 1.0) {
+  return data => {
+    const yDomain = getYDomain(data)
+    const yScale = d3.scaleLinear().range([height, 0]).domain(yDomain)
+    yAxisEl.call(yAxis.scale(yScale))
+    path.datum(data).attr('d', line.y(d => yScale(d[1])))
+
     // Create a copy of the line data and add a single data point at which
     // forces the y value to 0 and will enable the path to be safely filled.
     const areaData = data.slice()
     areaData.unshift([xDomain[0], 0.0])
     areaData.push([xDomain[1], 0.0])
-    g.append('path')
-      .datum(areaData)
-      .attr('d', line)
-      .attr('stroke-width', 0)
-      .attr('fill', 'red')
-      .attr('clip-path', 'url(#' + clipId + ')')
+    // Set the fill path.
+    fill.datum(areaData).attr('d', line).attr('clip-path', `url(#clip-${index})`)
 
-    g.append('line')
-      .attr('x1', xScale(xDomain[0]))
-      .attr('y1', yScale(1))
-      .attr('x2', xScale(xDomain[1]))
-      .attr('y2', yScale(1))
-      .attr('stroke', 'red')
-      .attr('stroke-dasharray', 4)
-    g.append('line')
-      .attr('x1', xScale(xDomain[0]))
-      .attr('y1', yScale(-1))
-      .attr('x2', xScale(xDomain[1]))
-      .attr('y2', yScale(-1))
-      .attr('stroke', 'red')
-      .attr('stroke-dasharray', 4)
+    const redLine = {
+      yPlus1: yScale(1.0),
+      yMinus1: yScale(-1.0),
+      x1: xScale(xDomain[0]),
+      x2: xScale(xDomain[yDomain[0] < -1.0 || yDomain[1] > 1.0 ? 1.0 : 0.0])
+    }
+    clipPos.attr('y', yScale(yDomain[1])).attr('height', redLine.yPlus1 - yScale(yDomain[1]))
+    clipNeg.attr('y', redLine.yMinus1).attr('height', yScale(yDomain[0]) - redLine.yMinus1)
+    maxLine
+      .attr('x1', redLine.x1).attr('y1', redLine.yPlus1)
+      .attr('x2', redLine.x2).attr('y2', redLine.yPlus1)
+    minLine
+      .attr('x1', redLine.x1).attr('y1', redLine.yMinus1)
+      .attr('x2', redLine.x2).attr('y2', redLine.yMinus1)
   }
-  g.append('path')
-    .datum(data)
-    .attr('d', line)
-    .attr('stroke', 'black')
-    .attr('stroke-width', 1.5)
-    .attr('fill', 'none')
-  g.append('g')
-    .classed('axis', true)
-    .attr('transform', 'translate(0, ' + internalHeight / 2 + ')')
-    .call(xAxis)
-  g.append('g')
-    .classed('axis', true)
-    .call(yAxis)
 }
 
 function setValues (input) {
   input.frequency.value.innerText = input.frequency.range.value
   input.amplitude.value.innerText = input.amplitude.range.value
-  input.phase.value.innerText = input.phase.range.value
+  input.phase.value.innerText = radiansToUser(input.phase.range.value)
 }
 
+const width = 650 / 2
+const margin = { top: 12, left: 30, bottom: 12, right: 4 }
+const internalWidth = width - margin.left - margin.right
+
 export function mixerPage () {
-  const input1 = {
+  const input = [{
     frequency: { range: document.querySelector('#frequency1'), value: document.querySelector('#freqvalue1') },
     amplitude: { range: document.querySelector('#amplitude1'), value: document.querySelector('#ampvalue1') },
     phase: { range: document.querySelector('#phase1'), value: document.querySelector('#phasevalue1') }
-  }
-  const input2 = {
+  }, {
     frequency: { range: document.querySelector('#frequency2'), value: document.querySelector('#freqvalue2') },
     amplitude: { range: document.querySelector('#amplitude2'), value: document.querySelector('#ampvalue2') },
     phase: { range: document.querySelector('#phase2'), value: document.querySelector('#phasevalue2') }
-  }
+  }]
 
   const points = internalWidth / 2
   const makeXArray = () => d3.range(0, points).map(k => k / (points - 1) * Math.PI * 2)
   const yValue = input => x => Math.sin((x + parseFloat(input.phase.range.value)) * parseFloat(input.frequency.range.value)) * parseFloat(input.amplitude.range.value)
   const makePoints = f => x => [x, f(x)]
 
+  const c1 = yValue(input[0])
+  const c2 = yValue(input[1])
+
+  const g1 = sine('#graph1', 1)
+  const g2 = sine('#graph2', 2)
+
+  const g3 = sine('#graphSum', 3)
+  const g4 = sine('#graphSatSum', 4)
+  const g5 = sine('#graphModSum', 5)
+
   const update = () => {
-    setValues(input1)
-    setValues(input2)
-
-    const c1 = yValue(input1)
-    const c2 = yValue(input2)
-
-    sine('#graph1', makeXArray().map(makePoints(c1)))
-    sine('#graph2', makeXArray().map(makePoints(c2)))
-
-    sine('#graphSum', makeXArray().map(x => [x, c1(x) + c2(x)]))
-    sine('#graphSatSum', makeXArray().map(x => [x, Math.max(Math.min(c1(x) + c2(x), 1.0), -1.0)]))
-    sine('#graphModSum', makeXArray().map(x => [x, (c1(x) + c2(x)) % 1.0]))
+    setValues(input[0])
+    setValues(input[1])
+    g1(makeXArray().map(makePoints(yValue(input[0]))))
+    g2(makeXArray().map(makePoints(yValue(input[1]))))
+    g3(makeXArray().map(x => [x, c1(x) + c2(x)]))
+    g4(makeXArray().map(x => [x, Math.max(Math.min(c1(x) + c2(x), 1.0), -1.0)]))
+    g5(makeXArray().map(x => [x, (c1(x) + c2(x)) % 1.0]))
   }
 
   [
-    input1.frequency.range,
-    input1.amplitude.range,
-    input1.phase.range,
-    input2.frequency.range,
-    input2.amplitude.range,
-    input2.phase.range
+    input[0].frequency.range,
+    input[0].amplitude.range,
+    input[0].phase.range,
+    input[1].frequency.range,
+    input[1].amplitude.range,
+    input[1].phase.range
   ].forEach(el => el.addEventListener('input', update))
   update()
 }
